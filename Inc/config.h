901affeee0a3b1330ee99fc1145d85792bd91a8a
vanAken <39534216+vanAken@users.noreply.github.com>
2024-05-31 14:26:21 +0200

    Update config.h
    dms 2000 25kmh

--- Inc/config.h
+++ Inc/config.h
@@ -83,7 +83,7 @@
 //Speed settings
 #define WHEEL_CIRCUMFERENCE 2200
 #define GEAR_RATIO 23 //11 for BionX IGH3 23 für 46 Magnete für AYW für 48V 1000W Standard 11x6 ca. 360-380U/min
-#define SPEEDLIMIT 35
+#define SPEEDLIMIT 25
 #define PULSES_PER_REVOLUTION 1
 #define SPEEDSOURCE INTERNAL
 #define SPEEDFILTER 1
@@ -99,7 +99,7 @@
 
 //---------------------------------------------------------------------
 //torquesensor settings
-#define TS_COEF 2400
+#define TS_COEF 2000   // org. 2400 volker
 #define TS_MODE
 #define TQONAD1
 
