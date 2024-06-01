9b18e3785ddf01e9f4809805eb2b2d7865e10d95
vanAken <39534216+vanAken@users.noreply.github.com>
2024-06-01 16:30:20 +0200

    Update config.h
    org 14A BMS

--- Inc/config.h
+++ Inc/config.h
@@ -92,7 +92,7 @@
 //---------------------------------------------------------------------
 //power settings
 #define PH_CURRENT_MAX 1200
-#define BATTERYCURRENT_MAX 25000  // org. 14 000 volker 25 000
+#define BATTERYCURRENT_MAX 14000  // org. 14 000 volker 25 000
 #define REVERSE 1 //1 for normal direction, -1 for reverse
 #define PUSHASSIST_CURRENT 300
 #define VOLTAGE_MIN 1320 //33V
