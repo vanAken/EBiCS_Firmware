4996009e523ce37a52b7a0f45fadd007cd01c3f1
vanAken <39534216+vanAken@users.noreply.github.com>
2024-06-15 18:34:44 +0200

    Update config.h
    THROTTLE_MID

--- Inc/config.h
+++ Inc/config.h
@@ -76,7 +76,9 @@
 
 //---------------------------------------------------------------------
 //Throttle settings
-#define THROTTLE_OFFSET 1600   //default value: 690 or 1850 for Bidirektional THROTTLE grip - Wheelchair puller branch has 1770   #volker                           
+#define THROTTLE_MIN 700        // org. 7debug my value later TODO 
+#define THROTTLE_OFFSET 1600   //default value: 690 or 1850 for Bidirektional THROTTLE grip - Wheelchair puller branch has 1770   #volker   
+#define THROTTLE_MID THROTTLE_OFFSET
 #define THROTTLE_MAX 2630  // 2630 (from debug, hterm)    // org. 2850
 #define THROTTLE_OVERRIDE
 
