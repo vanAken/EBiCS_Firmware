cd8b89c2587ad2b78e2b236f5cf91aea9b5f487c
vanAken <39534216+vanAken@users.noreply.github.com>
2024-09-14 19:26:07 +0200

    Update config.h
    adc ofcoment

--- Inc/config.h
+++ Inc/config.h
@@ -114,7 +114,7 @@
 
 #define REGEN_CURRENT 800
 #define REGEN_CURRENT_MAX 4000  // org. 10 000  volker
-#define ADC_BRAKE
+//#define ADC_BRAKE
 
 //---------------------------------------------------------------------
 #define AUTODETECT 0  //  org. 0  
